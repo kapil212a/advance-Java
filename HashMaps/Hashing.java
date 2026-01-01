56f1e2d41f5718c3578c11c386b880f3cb669214
kapil212a <amangupta74086@gmail.com>
2026-01-01 17:22:26 +0530

    add

---
+++ HashMaps/Hashing.java
@@ -0,0 +1,16 @@
+package HashMaps;
+import java.util.*;;
+
+public class Hashing {
+    public static void main (String args[]){
+        HashMap<String,Integer> hm = new HashMap<>();
+
+        hm.put("India" , 150);
+        hm.put("Nepal" , 50);
+        hm.put("China" , 160);
+
+        System.out.println(hm);
+    }
+    
+    
+}
