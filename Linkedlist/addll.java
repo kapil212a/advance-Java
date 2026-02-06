9c3701f061b3fa6dc19fed0e14040b3042e0b14d
kapil212a <amangupta74086@gmail.com>
2026-02-06 20:25:51 +0530

    add

---
+++ Linkedlist/addll.java
@@ -0,0 +1,30 @@
+package Linkedlist;
+import java.util.*;
+
+public class addll {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static void addfirst(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+    }
+}
