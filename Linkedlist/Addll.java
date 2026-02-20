da39ac691dbec4d23a340d84da8c342f28cd0a3b
kapil212a <amangupta74086@gmail.com>
2026-02-20 22:20:18 +0530

    add

--- Linkedlist/Addll.java
+++ Linkedlist/Addll.java
@@ -216,7 +216,30 @@ public class Addll {
         return false;
     }
 
-    
+    public static void removeCycle(){
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                cycle = true;
+                break;
+            }
+        }
+        if(cycle == false){
+            return;
+        }
+        slow = head;
+        Node prev = null;
+        while(slow != fast){
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        prev.next = null;
+    }
     
 
     public static void main(String[] args) {
